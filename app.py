d9d33b144c728ccb37ec937fc8ddbb44a8e2387f
Pooja Pandey <09Pooja99@gmail.com>
2025-02-13 16:55:29 +0530

    Simple flask app

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from flask import Flask 
-from thyroid_detection.logger import logging
-from thyroid_detection.exception import ThyroidException 
-import sys,os
+from flask import Flask
 
-app= Flask(__name__)
+app=Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
+
+@app.route("/",methods=['GET','POST'])
 def index():
-        try:
-              raise Exception("we are testing custom exception")
-        except Exception as e:
-            raise Exception(e,sys)
-            logging.info(thyroid_detection.error_messege)
-            logging.info("We are testing logging module")
-        return "Machine Learning Project"
+    return "Starting Machine Learning Project"
 
-if __name__ == "__main__":
-    app.run(debug= True)
+
+if __name__=="__main__":
+    app.run(debug=True)
